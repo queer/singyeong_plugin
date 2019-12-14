0ee19fe4bc6c4890b22b92b89aa0bc3b139daab9
amy null <null@amy.gg>
2019-12-14 15:34:47 -0500

    nil out website manifest field

--- lib/singyeong_plugin/manifest.ex
+++ lib/singyeong_plugin/manifest.ex
@@ -4,7 +4,7 @@ defmodule Singyeong.Plugin.Manifest do
   typedstruct do
     field :name, String.t(), enforce: true
     field :description, String.t(), default: "My cool plugin."
-    field :website, String.t()
+    field :website, String.t() | nil, default: nil
     field :events, [String.t()] | [], default: []
   end
 end
