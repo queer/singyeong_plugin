b7e692a4fbe60620eef05ff2cf76b0dc58fbb5b9
amy null <null@amy.gg>
2019-12-14 15:25:09 -0500

    Add undo callback

--- lib/singyeong_plugin.ex
+++ lib/singyeong_plugin.ex
@@ -37,21 +37,45 @@ defmodule Singyeong.Plugin do
   will never be passed to this function.
 
   Plugin event handling behaves in a middleware-type fashion. A plugin can
-  choose to pass execution to the next plugin in the chain via `:next`. halt
+  choose to pass execution to the next plugin in the chain via `:next`, halt
   execution with `:halt`, or return an error. Returning an error immediately
-  halts execution and unwinds as much as possible.
+  halts execution and unwinds as much as possible. Halting will immediately
+  stop processing of the event, and will not return anything to the client, as
+  it is assumed that the halted execution was not meant to return any
+  side-effects to the client.
 
-  In the non-error cases, the third return element is the undo state. This is
-  some state that the plugin uses to undo any stateful changes it may have made
-  during the processing of this event, such that any error can result in a
-  rollback of stateful changes. Pure functions may simply return `nil`.
+  On success, this function returns a tuple starting with either `:next` or
+  `:halt`
+
+  In all cases, the third return element is the undo state. This is some state
+  that the plugin uses to undo any stateful changes it may have made during the
+  processing of this event, such that any error can result in a rollback of
+  stateful changes. Pure functions may simply return `nil` for the undo state,
+  or just not include it as an element of the tuple. **Note that a `nil` undo
+  state is assumed to mean that this function has *nothing* to undo, and as
+  such the undo function for this module will *never* be called if the event
+  handled by this function requires undo!** That is, **returning an undo state of
+  `nil` means that your undo function will never be called**. The reason for
+  this is simply that **the order in which `handle_event` vs `undo` is called
+  cannot be guaranteed**, and so you cannot rely on strict ordering of these
+  function calls for reliable undo behaviour.
 
   # TODO: Provide a real interface for listening on all events.
   """
   @callback handle_event(binary(), any()) ::
-              {:next, list(frame()), undo_state()}
-              | {:halt, list(frame()), undo_state()}
+              {:next, [frame()], undo_state()}
+              | {:next, [frame()]}
+              | {:halt, [frame()], undo_state()}
+              | {:halt, [frame()]}
               | {:error, binary(), undo_state()}
+              | {:error, binary()}
 
-  # TODO: Provide a real callback to undo with
+  @doc """
+  Called when this plugin is supposed to undo some stateful changes.
+  Implementations of this function are *expected* to be impure, and may do more
+  or less whatever they want. The `undo_state` parameter may be **any** value,
+  **with the exception that a `nil` undo state will never be passed to this
+  function, for any reason**.
+  """
+  @callback undo(binary(), undo_state()) :: :ok | {:error, binary()}
 end
