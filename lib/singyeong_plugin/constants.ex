c8784280d437d063694351077fe70146516eccb5
amy null <null@amy.gg>
2020-01-17 23:24:47 -0500

    Prefer dynamic function call via apply/3 over defdelegate

--- lib/singyeong_plugin/constants.ex
+++ lib/singyeong_plugin/constants.ex
@@ -1,8 +1,12 @@
 defmodule Singyeong.Plugin.Constants do
   @spec gateway_opcodes() :: %{integer() => atom()}
-  defdelegate gateway_opcodes, to: Singyeong.Gateway, as: :opcodes_id
+  def gateway_opcodes do
+    apply gateway_module(), :opcodes_id, []
+  end
   @spec gateway_opcodes_by_name() :: %{atom() => integer()}
-  defdelegate gateway_opcodes_by_name, to: Singyeong.Gateway, as: :opcodes_name
+  def gateway_opcodes_by_name do
+    apply gateway_module(), :opcodes_name, []
+  end
 
   defp gateway_module, do: Application.get_env :singyeong_plugin, :gateway_module
 end
