6d5e5ba2d3eba7b31b554c3596687b5b59b5e316
amy null <null@amy.gg>
2020-03-12 14:16:07 -0400

    Add route methods

--- lib/singyeong_plugin/rest_route.ex
+++ lib/singyeong_plugin/rest_route.ex
@@ -2,6 +2,7 @@ defmodule Singyeong.Plugin.RestRoute do
   use TypedStruct
 
   typedstruct do
+    field :method, atom(), enforce: true
     field :route, String.t(), enforce: true
     field :module, atom(), enforce: true
     field :function, atom(), enforce: true
