456dad1dd54155e358a616b9631af0b9e5c8adca
amy null <null@amy.gg>
2020-03-18 12:03:29 -0400

    Add ex_doc, whoops

--- mix.exs
+++ mix.exs
@@ -47,6 +47,7 @@ defmodule SingyeongPlugin.MixProject do
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
       {:typed_struct, "~> 0.1.4"},
       {:plug, "~> 1.9"},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
     ]
   end
 end
