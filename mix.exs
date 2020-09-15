be49b1309694b7e03b36503dbf33e7a3970d3769
amy null <null@amy.gg>
2020-09-15 19:32:33 -0400

    0.1.2

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule SingyeongPlugin.MixProject do
   use Mix.Project
 
-  @version "0.1.1"
+  @version "0.1.2"
   @repo_url "https://github.com/queer/singyeong_plugin"
 
   def project do
@@ -44,7 +44,7 @@ defmodule SingyeongPlugin.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-      {:typed_struct, "~> 0.1.4"},
+      {:typed_struct, "~> 0.2.1"},
       {:plug, "~> 1.9"},
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
     ]
