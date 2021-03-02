d87e4cf2237dadd380c78e7cb965476c6bc5cca1
amy null <null@amy.gg>
2021-03-02 05:17:43 -0500

    Hoist up some parts of the server API

---
+++ lib/singyeong/store/client.ex
@@ -0,0 +1,16 @@
+defmodule Singyeong.Store.Client do
+  @moduledoc false
+
+  use TypedStruct
+  alias Singyeong.Store
+
+  typedstruct enforce: true do
+    field :app_id, Store.app_id()
+    field :client_id, Store.client_id()
+    field :metadata, map()
+    field :metadata_types, map()
+    field :socket_pid, pid()
+    field :socket_ip, String.t() | nil
+    field :queues, [atom()]
+  end
+end
